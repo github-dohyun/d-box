50139411c6dcec347cba9f032a844a6032df2cc2
kimdohyun <github.dohyun@gmail.com>
2018-01-01 03:10:35 +0900

    구구단 미완

---
+++ practice C/2. for문 사용하기/2. for문 사용하기/소스.c
@@ -0,0 +1,19 @@
+//for문을 사용해서 구구단 출력하기 //미완
+#include <stdio.h>
+
+int main()
+{
+	int f, l; //f는 first l은 last
+
+	 for (f = 1; f < 10; f++)
+	   {
+		 printf("%d단 =")
+
+	   }
+
+
+
+
+
+	return 0;
+}
